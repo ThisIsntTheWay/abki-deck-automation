6daa2a5958c5e15ed8ad889f1439a06faf58e84b
ThisIsntTheWay <msblauwal6@gmail.com>
2024-02-27 19:31:16 +0100

    Fix export path missing in run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,14 @@ set -e
 
 HOST=$([ "$1" ] && echo "$1" || echo "localhost:8765")
 ANKI_FOLDER=$([ "$2" ] && echo "$2" || echo "anki")
+EXPORT_PATH=$([ "$3" ] && echo "$3" || echo "anki.apkg")
 
-echo "$HOST - $ANKI_FOLDER"
+if ! [[ $EXPORT_PATH =~ ".apkg" ]]; then
+    echo "[X] EXPORT_PATH ($EXPORT_PATH) is not an .apkg file."
+    exit 1
+fi
+
+echo "Rendering '$ANKI_FOLDER' to '$EXPORT_PATH' with AnkiConnect at '$HOST'."
 
 python check.py "$ANKI_FOLDER"
-python assemble.py "$HOST" "$ANKI_FOLDER"
+python assemble.py "$HOST" "$EXPORT_PATH"
